dac8385720e17d9596338ab6cde680d6eba099f8
AhmadNidzom <thecrimsen99@gmail.com>
2024-12-08 15:58:02 +0700

    Update Gaji.java

--- Gaji.java
+++ Gaji.java
@@ -2,6 +2,28 @@
 
 public class Gaji{
     public static int masaKerja;
+    static double GajiPokok(){
+        double gajiPokok;
+        if (masaKerja<5) {
+            gajiPokok=5000000;
+        }else if (masaKerja >=5 || masaKerja<10) {
+            gajiPokok=7500000;
+        }else{
+            gajiPokok=10000000;
+        }
+        return gajiPokok;
+    }
+    static double GajiLembur(int jamLembur){
+        double gajiLembur;
+        if (masaKerja<5) {
+            gajiLembur=jamLembur*40000;
+        }else if (masaKerja>=5 || masaKerja <10) {
+            gajiLembur=jamLembur*50000;
+        }else{
+            gajiLembur=jamLembur*75000;
+        }
+        return gajiLembur;
+    }
     public static void main(String[] args){
 
         Scanner input = new Scanner(System.in);
@@ -9,7 +31,10 @@ public class Gaji{
         int jamLembur = input.nextInt();
 
         /* Pekerjaan anda mulai dari sini */
-
+        double gajiPokok = GajiPokok();
+        double gajiLembur = GajiLembur(jamLembur);
+        double totalGaji = gajiPokok + gajiLembur;
+        System.out.println(totalGaji);
         /* Pekerjaan anda berakhir sini */
 
         System.out.println(Gaji.class.getDeclaredMethods().length);
